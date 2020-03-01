6f480e7cc07abe67a1716d0fb7d0095a45d024e1
Nelson Díaz <diaznelson23@gmail.com>
2020-03-01 13:02:25 -0600

    Comentarios

--- ProyectoIng/account/urls.py
+++ ProyectoIng/account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateUser,TemplateLogin,logout_view, activate
+from .views import * #CreateUser,TemplateLogin,logout_view, activate
 
 urlpatterns = [
     path('signup',CreateUser.as_view(), name='signup'),#Template Registro
     path('login',TemplateLogin.as_view(), name='login'),#Template Login
     path('logout',logout_view, name='logout'),#Logout
     path('activate/<uidb64>/<token>',activate, name='activate'),#Activate account
+    #path('url_bajo_account',vista en views, name='nombre'),
 ]
