f4b75560a7fd1dbea7c6270cc6fafb3a971097c7
jarjar-st <37054631+jarjar-st@users.noreply.github.com>
2020-03-17 11:15:03 -0600

    Revert "a probar"
    This reverts commit 8e6501413b50e82a7e76a9f2b87f608b4ecc5a2d.

--- ProyectoIng/ad/urls.py
+++ ProyectoIng/ad/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from .views import ShowAdsListView
-from .views import AdCreate
 
 urlpatterns = [
     path('', ShowAdsListView.as_view(), name='my_products'),
-    path('create/', AdCreate.as_view(), name='create'),
 ]
